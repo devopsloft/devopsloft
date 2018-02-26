9e7f66df3765201b9588ee0113bcb46df35e70b1
lioramilbaum <liora@lmb.co.il>
2018-02-26 16:32:52 +0200

    Home page #15
    Home page changed to present DevOps Loft README

--- www/index.js
+++ www/index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', function(req, res) {
+  // res.send('Hello World!');
+  res.sendFile('public/home.html', {root: __dirname} )
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
