949f6cf519d311b4d95b52096631182d0aafd449
Liora Milbaum <liora@lmb.co.il>
2018-12-07 23:43:06 +0200

    Get events function

---
+++ publish/events.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import meetup.api
+import json
+import os
+
+def getApiKey():
+    fname = '.secrets'
+    if not os.path.isfile(fname):
+        exit
+
+    with open(fname) as secret:
+        api_key = secret.read().strip()
+        return api_key
+
+def getEvents(status):
+    events = client.GetEvents({'group_urlname': 'DevOps-Loft', 'status': status})
+    events = json.dumps(events.__dict__)
+
+    events = json.loads(events)
+
+    for event in events['results']:
+        print(event['name'])
+
+client = meetup.api.Client(getApiKey())
+getEvents(status='past')
