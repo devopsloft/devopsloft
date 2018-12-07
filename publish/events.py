223405d7cf854992ae60462a08116b771ffa1ae6
Liora Milbaum <liora@lmb.co.il>
2018-12-07 23:51:35 +0200

    fixing flake8 errors

--- publish/events.py
+++ publish/events.py
@@ -3,6 +3,7 @@ import meetup.api
 import json
 import os
 
+
 def getApiKey():
     fname = '.secrets'
     if not os.path.isfile(fname):
@@ -12,14 +13,17 @@ def getApiKey():
         api_key = secret.read().strip()
         return api_key
 
+
 def getEvents(status):
-    events = client.GetEvents({'group_urlname': 'DevOps-Loft', 'status': status})
+    events = client.GetEvents({'group_urlname': 'DevOps-Loft',
+                               'status': status})
     events = json.dumps(events.__dict__)
 
     events = json.loads(events)
 
     for event in events['results']:
         print(event['name'])
+
 
 client = meetup.api.Client(getApiKey())
 getEvents(status='past')
