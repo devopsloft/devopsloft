780e2b084d1d70f6d3cc659675b7b325f8af246e
k2view <k2view@localhost.localdomain>
2018-12-18 23:03:01 +0200

    blank lines

--- workshops/docker102/solutions/03/app.py
+++ workshops/docker102/solutions/03/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from a Docker container!'
+    return 'Hello from a Docker container!!'
 
 
 if __name__ == "__main__":
