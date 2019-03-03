2821b950b6764cb6672050d7ee3df2efaf0862e7
Liora Milbaum <liora@lmb.co.il>
2019-03-03 06:47:57 +0200

    Members Management - Sign Up front end + back end

--- create_db.sql
+++ create_db.sql
@@ -1,14 +1,10 @@
-
 CREATE DATABASE IF NOT EXISTS devopsloft;
+USE devopsloft;
 
-CREATE TABLE IF NOT EXISTS devopsloft.users 
-(id integer NOT NULL AUTO_INCREMENT, 
- first_name varchar(100) NOT NULL,
- last_name varchar(100), 
- email VARCHAR(320) NOT NULL,
- member_type varchar(30),
- allow_receive_emails boolean,
+CREATE TABLE IF NOT EXISTS users
+(id INT(11) AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL,
+ username VARCHAR(30),
  password varchar(100),
- password_last_change_date date,
- member_status varchar(20) comment 'values: active/blocked/cancelled/...',
- PRIMARY KEY (id));
+ signup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
