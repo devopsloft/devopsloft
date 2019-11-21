be4bcfba8900a76cdd30d7d6a777e34c1e842f8f
ldobrzinsky <ldobrzinsky@paloaltonetworks.com>
2019-11-21 13:32:31 +0200

    dockerfile initial
    fixing locations
    
    fixxed first solution dockerfile
    
    finishing docker documentation
    
    fixing readibility issues and adding destroy option
    
    fixing volume shares
    
    removing temporary SSL WA that was previously commited by mistake
    
    fixing readme to generic path
    
    fix pep8 formatting issues
    
    changin volume mount to COPY
    
    dockerfile initial
    
    fixing locations
    
    fixxed first solution dockerfile
    
    finishing docker documentation
    
    fixing readibility issues and adding destroy option
    
    fixing volume shares
    
    removing temporary SSL WA that was previously commited by mistake
    
    fixing readme to generic path
    
    fix pep8 formatting issues
    
    fixing documentation issues
    
    documentation for destroying

---
+++ spin-docker.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import dotenv
+import os
+import subprocess
+import click
+import in_docker
+from createPemFiles import SelfSignedCertificate, IsCertExist
+
+
+print_debug = 'No'
+
+
+def print_info(message):
+    global print_debug
+    if print_debug == 'yes':
+        print("--- python debug ---> ", message)
+
+
+def PrepareEnvironmentVars(environmentName, action):
+    # Reads the .env file from the repository
+    # Returns an array with all the env vars, inclduing modificatoins per env
+<<<<<<< HEAD
+	
+    dotenv.load_dotenv()
+    envArray = os.environ.copy()
+    envArray['RUN_BY_PYTHON'] = 'yes'
+=======
+
+    dotenv.load_dotenv()
+    envArray = os.environ.copy()
+    envArray['RUN_BY_PYTHON'] = 'yes'
+    envArray['BASE_FOLDER'] = in_docker.get_binds()['/resources']
+>>>>>>> 379abf1... dockerfile initial
+    envArray['ENVIRONMENT'] = environmentName
+    if (environmentName == 'dev'):
+
+        envArray['WEB_HOST_PORT'] = envArray['DEV_WEB_HOST_PORT']
+        envArray['WEB_GUEST_PORT'] = envArray['DEV_WEB_GUEST_PORT']
+        envArray['WEB_HOST_SECURE_PORT'] = envArray['DEV_WEB_HOST_SECURE_PORT']
+        envArray['WEB_GUEST_SECURE_PORT'] = envArray['DEV_WEB_GUEST_SECURE_PORT']
+    return envArray
+
+
+@click.command()
+@click.option("-e", "--environment", required=False, default="dev",
+                    type=click.Choice(["dev", "prod", "stage"]))
+@click.option("-a", "--action", required=False, default="up",
+                    type=click.Choice(["up", "destroy"]))
+@click.option("-d", "--debug", required=False, default="no",
+                    type=click.Choice(["yes", "no"]))
+def main(environment, action, debug):
+    machineName = environment
+    envVars = machineName
+    envVars = PrepareEnvironmentVars(envVars, action)
+    if not (IsCertExist()):
+        SelfSignedCertificate()
+    if(action == "up"):
+        command = "docker-compose up -d"
+        subprocess.Popen(command, env=envVars, shell=True)
+    if(action == "destroy"):
+        command = "docker-compose down -v --rmi all --remove-orphans"
+        subprocess.Popen(command, env=envVars, shell=True)
+
+
+if __name__ == '__main__':
+    main()
